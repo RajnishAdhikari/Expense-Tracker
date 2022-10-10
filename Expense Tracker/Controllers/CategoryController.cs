43a5ceec0a722805c8fccefa1c1241be62def229
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-10-10 22:58:14 +0545

    transaction controller created and modified

--- Expense Tracker/Controllers/CategoryController.cs
+++ Expense Tracker/Controllers/CategoryController.cs
@@ -23,37 +23,21 @@ namespace Expense_Tracker.Controllers
         {
             return _context.Categories != null ?
                         View(await _context.Categories.ToListAsync()) :
-                        Problem("Entity set 'ApplicationDbContext.Categories' is null.");
+                        Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
 
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
-        // GET: Category/Add or edit
+        // GET: Category/AddOrEdit
         public IActionResult AddOrEdit(int id = 0)
         {
-            if(id == 0)
+            if (id == 0)
                 return View(new Category());
             else
                 return View(_context.Categories.Find(id));
+
         }
 
-        // POST: Category/Add or edit
+        // POST: Category/AddOrEdit
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
@@ -66,31 +50,12 @@ namespace Expense_Tracker.Controllers
                     _context.Add(category);
                 else
                     _context.Update(category);
-
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(category);
         }
 
-       
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
 
         // POST: Category/Delete/5
         [HttpPost, ActionName("Delete")]
@@ -106,14 +71,10 @@ namespace Expense_Tracker.Controllers
             {
                 _context.Categories.Remove(category);
             }
-            
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CategoryExists(int id)
-        {
-          return _context.Categories.Any(e => e.Id == id);
-        }
     }
 }
